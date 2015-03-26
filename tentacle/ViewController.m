98f41235670b5189b8ec175df97e7446a71dcfa0
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2015-03-26 10:01:37 +0800

    Initial Commit

---
+++ tentacle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tentacle
+//
+//  Created by lingminjun on 15/3/26.
+//  Copyright (c) 2015年 SF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
